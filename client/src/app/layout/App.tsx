85f518d13624c15efc86d6471d60477b59b6ff78
hypnoz3 <erno.karppinen@gmail.com>
2021-11-05 15:08:26 +0200

    Adding client side

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import { Container, CssBaseline } from "@mui/material";
+import { useState } from "react";
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  }) 
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+     <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+     <Container>
+     <Catalog />
+     </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+
---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import { Fragment } from "react";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+    products: Product[];
+}
+
+export default function ProductList({products}: Props) {
+    return (
+    <Fragment>
+    <Grid container spacing={4}>
+       {products.map(product => (
+           <Grid item xs={3} key={product.id}>
+                <ProductCard key={product.id} product={product} />
+           </Grid>
+         
+       ))}
+     </Grid>
+
+     </Fragment>
+     )
+}
