b80881c4447f43acd3a6e98bedebd84ebc4bb261
hypnoz3 <erno.karppinen@gmail.com>
2021-11-08 11:02:54 +0200

    added routing and product details

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,9 +4,15 @@ import createTheme from '@mui/material/styles/createTheme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
+import { Route, Routes } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 
-function App() {
+
+function App(): JSX.Element {
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -28,7 +34,13 @@ function App() {
       <CssBaseline />
      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
      <Container>
-     <Catalog />
+       <Routes>
+     <Route path='/' element={<HomePage/>} />
+     <Route path='/catalog' element={<Catalog/>} />
+     <Route path='/catalog/:id' element={<ProductDetails/>} />
+     <Route path='/about' element={<AboutPage/>} />
+     <Route path='/contact' element={<ContactPage/>} />
+     </Routes>
      </Container>
     </ThemeProvider>
   );
