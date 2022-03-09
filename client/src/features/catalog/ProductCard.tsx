1839615f71b47f13090c178b00add7265cb8c7dc
hypnoz3 <erno.karppinen@gmail.com>
2022-03-09 12:58:21 +0200

    Final Redux update

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,12 +1,10 @@
 import { LoadingButton } from "@mui/lab";
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product";
-import { useAppDispatch } from "../../app/store/configureStore";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import { currencyFormat } from "../../app/util/utli";
-import { setBasket } from "../basket/basketSlice";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 
 interface Props {
@@ -14,16 +12,10 @@ interface Props {
 }
 
 export default function ProductCard({product}: Props) {
-  const [loading, setLoading] = useState(false);
+  const {status} = useAppSelector(state => state.basket);
   const dispatch = useAppDispatch();
 
-  function handleAddItem(productId: number) {
-    setLoading(true);
-    agent.Basket.addItem(productId)
-      .then(basket => dispatch(setBasket(basket)))
-      .catch(error => console.log(error))
-      .finally(() => setLoading(false));
-  }
+
     return (
         
         <Card >
@@ -52,7 +44,7 @@ export default function ProductCard({product}: Props) {
           </Typography>
         </CardContent>
         <CardActions>
-          <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
+          <LoadingButton loading={status.includes('pendingAddItem' + product.id)} onClick={() => dispatch(addBasketItemAsync({productId: product.id}))} size="small">Add to cart</LoadingButton>
           <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
         </CardActions>
       </Card>
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,9 +1,6 @@
-import { Typography } from "@mui/material";
 
 export default function HomePage() {
     return (
-        <Typography variant='h2'>
-            Home Page
-        </Typography>
+        <img src="" alt="" />
     )
 }
