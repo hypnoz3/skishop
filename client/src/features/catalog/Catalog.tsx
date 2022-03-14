543d276bb775a9106b5cf30ecacdea7d1ae69593
hypnoz3 <erno.karppinen@gmail.com>
2022-03-14 15:24:56 +0200

    catalog page update

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,24 +1,87 @@
-import { Fragment, useEffect } from "react";
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Pagination, Paper, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
+
+const sortOptions = [
+    {value: 'name', label: 'Alphabetical'},
+    {value: 'priceDesc', label: 'Price - High to low'},
+    {value: 'priceAsc', label: 'Price - Low to high'}
+]
 
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog)
+    const {productsLoaded, status, filtersLoaded, brands, types} = useAppSelector(state => state.catalog)
     const dispatch = useAppDispatch();
     
 
   useEffect(() => {
-    if (!productsLoaded) dispatch(fetchProductsAsync());
+    if (!productsLoaded) dispatch(fetchProductsAsync()); 
 }, [productsLoaded, dispatch])
+
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded])
 
 if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
     return (
-        <Fragment>
-        <ProductList products={products} />
-     </Fragment>
+        <Grid container spacing={4}>
+            <Grid item xs={3}>
+            <Paper sx={{mb: 2}}>
+                <TextField
+                    label='Search products'
+                    variant='outlined'
+                    fullWidth
+                >
+
+                </TextField>
+            </Paper>
+            <Paper sx={{mb: 2, p: 2}}>
+            <FormControl>
+                <RadioGroup>
+                    {sortOptions.map(({value, label}) => (
+                        <FormControlLabel value={value} control={<Radio />} label={label} key={value} />
+                    ))}
+                </RadioGroup>
+                </FormControl>
+            </Paper>
+            <Paper sx={{mb: 2, p: 2}}>
+                <FormGroup>
+                    {brands.map(brand => (
+                        <FormControlLabel control={<Checkbox  />} label={brand} key={brand} />
+                    ))}
+                </FormGroup>
+            </Paper>
+            <Paper sx={{mb: 2, p: 2}}>
+                <FormGroup>
+                    {types.map(type => (
+                        <FormControlLabel control={<Checkbox  />} label={type} key={type} />
+                    ))}
+                </FormGroup>
+            </Paper>
+            </Grid>
+            <Grid item xs={9}>
+            <ProductList products={products} />
+            </Grid>
+            <Grid item xs={3}/>
+            <Grid item xs={9}>
+                <Box display='flex' justifyContent='space-between' alignItems='center'>
+                    <Typography>
+                        Displaying 1-6 of 20 itmes
+                    </Typography>
+                    <Pagination
+                        color='secondary'
+                        size='large'
+                        count={10}
+                        page={2}
+
+                    />
+                </Box>
+            </Grid>
+     </Grid>
     )
 }
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -27,12 +27,26 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
     }
 )
 
+export const fetchFilters = createAsyncThunk(
+    'catalog/fetchFilters',
+    async (_, thunkApi) => {
+        try {
+            return agent.Catalog.fetchFilters();
+        } catch (error: any) {
+            return thunkApi.rejectWithValue({error: error.data});
+        }
+    }
+)
+
 export const catalogSlice = createSlice({
     name: 'catalog',
     initialState: productsAdapter.getInitialState({
         productsLoaded: false,
-        status: 'idle'
-    }),
+        filtersLoaded: false,
+        status: 'idle',
+        brands: [],
+        types: []
+        }),
     reducers: {},
     extraReducers: (builder => {
         builder.addCase(fetchProductsAsync.pending, (state) => {
@@ -57,7 +71,20 @@ export const catalogSlice = createSlice({
         builder.addCase(fetchProductAsync.rejected, (state, action) => {
             console.log(action);
             state.status = 'idle';
-        })
+        });
+        builder.addCase(fetchFilters.pending, (state) => {
+            state.status = 'pendingFetchFilters';
+        });
+        builder.addCase(fetchFilters.fulfilled, (state, action) => {
+            state.brands = action.payload.brands;
+            state.types = action.payload.types;
+            state.filtersLoaded = true;
+            state.status = 'idle';
+        });
+        builder.addCase(fetchFilters.rejected, (state, action) => {
+            state.status = 'idle';
+            console.log(action.payload);
+        });
     })
 })
 
