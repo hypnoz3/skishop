53faac896c0772198773deb278b5124cf2e07390
hypnoz3 <erno.karppinen@gmail.com>
2022-03-22 08:45:05 +0200

    Adding identity

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import createTheme from '@mui/material/styles/createTheme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
@@ -14,30 +14,31 @@ import 'react-toastify/dist/ReactToastify.css';
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
-import { getCookie } from "../util/utli";
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import { useAppDispatch } from "../store/configureStore";
-import { setBasket } from "../../features/basket/basketSlice";
+import { fetchBasketAsync } from "../../features/basket/basketSlice";
 import Login from "../../features/account/Login";
 import Register from "../../features/account/Register";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const buyerId = getCookie('buyerId');
-    if (buyerId) {
-      agent.Basket.get()
-        .then(basket => dispatch(setBasket(basket)))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-    } else {
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error: any) {
+      console.log(error);
     }
   }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false))
+  }, [initApp])
 
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
@@ -73,7 +74,7 @@ function App() {
      <Route path='/contact' component={ContactPage} />
      <Route path='/server-error' component={ServerError} />
      <Route path='/basket' component={BasketPage} />
-     <Route path='/checkout' component={CheckoutPage} />
+     <PrivateRoute path='/checkout' component={CheckoutPage} />
      <Route path='/login' component={Login} />
      <Route path='/register' component={Register} />
      <Route component={NotFound} />
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
     darkMode: boolean;
@@ -23,6 +24,7 @@ const navStyles = {color: 'inherit', typography: 'h6', '&:hover': {color: 'grey.
 
 export default function Header({darkMode, handleThemeChange}: Props) {
     const {basket} = useAppSelector(state => state.basket);
+    const {user} = useAppSelector( state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
 
 
@@ -55,6 +57,9 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                         <ShoppingCart/>
                     </Badge>
                 </IconButton>
+                {user ? (
+                    <SignedInMenu/>
+                ) : (
                 <List sx={{display: 'flex'}}>
                     {rightLinks.map(({title, path}) => (
                         <ListItem
@@ -67,6 +72,8 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                         </ListItem>
                     ))}
                 </List>
+                )}
+                
                 </Box>
              
 
